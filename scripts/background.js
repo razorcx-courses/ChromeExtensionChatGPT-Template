830f2fc58a657352e7aa7fe718a5641a92c7728b
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-06 20:49:44 -0800

    add comments to background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,12 @@
 // Background script
+
+// Listen for a click on the extension's icon.
 chrome.action.onClicked.addListener((tab) => {
+  // Execute the content script on the current tab.
   chrome.scripting.executeScript({
+    // Specify the target tab by its ID for the script to be injected.
     target: { tabId: tab.id },
+    // List of scripts to be injected into the tab.
     files: ["scripts/content.js"],
   });
 });
